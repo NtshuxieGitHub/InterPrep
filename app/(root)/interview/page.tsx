77c6b8e5fd2434355a7ea9d232faa2c7fac9bfb1
ntshuxekom98@gmail.com <ntshuxekom98@gmail.com>
2025-03-25 00:43:21 +0200

    integrate vapi with the UI on Agents.tsx

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
-const InterviewPage = () => {
+const InterviewPage = async () => {
+  const userInfo = await getCurrentUser();
+
   return (
     <>
       <h3>Practice Interview Generation</h3>
-      <Agent userName="Me" userId="user1" type="generate" />
+      <Agent userName={userInfo?.name} userId={userInfo?.id} type="generate" />
     </>
   );
 };
